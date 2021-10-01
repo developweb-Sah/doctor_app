b40f5a4488fabfcb1aa90bbb1e0b8566105640b5
SaheenJasmine <63956055+developweb-Sah@users.noreply.github.com>
2021-10-01 19:57:49 +0530

    Frontend of doctorapp

---
+++ src/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+import express from "express";
+import configViewEngine from "./config/viewEngine";
+import initWebRoutes from "./routes/web";
+import connectDB from "./config/connectDb.js";
+
+let app = express();
+
+//config view Engine
+configViewEngine(app);
+
+connectDB();
+//init all web routes
+initWebRoutes(app);
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`App is running at the port ${port}`);
+});
