e362945b060067c80a7a56092fbc7cacd1eaa99d
SaheenJasmine <63956055+developweb-Sah@users.noreply.github.com>
2021-10-02 11:30:35 +0530

    Register page

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -12,9 +12,39 @@ let createdNewUser = async (req, res) => {
   console.log(message);
   return res.redirect("/");
 };
+let getRegisterPage = (req, res) => {
+  return res.render("auth/register.ejs");
+};
+let getLoginPage = (req, res) => {
+  return res.render("auth/login.ejs");
+};
+let handleRegister = async (req, res) => {
+  //validate input fields
+
+  //create a new user
+  try {
+    let user = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+      createdAt: Date.now(),
+    };
+    // console.log(req.body);
+    let message = await userService.createNewUser(user);
+    console.log(message);
+    return res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   getHomepage: getHomepage,
   getNewUserPage: getNewUserPage,
   createdNewUser: createdNewUser,
+  getRegisterPage: getRegisterPage,
+  getLoginPage: getLoginPage,
+  handleRegister: handleRegister,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -9,6 +9,9 @@ let router = express.Router();
 
 let initAllWebRoutes = (app) => {
   router.get("/", homepageController.getHomepage);
+  router.get("/register", homepageController.getRegisterPage);
+  router.get("/login", homepageController.getLoginPage);
+  router.post("/register", homepageController.handleRegister);
   router.get("/new-user", homepageController.getNewUserPage);
   router.post("/create-new-user", homepageController.createdNewUser);
 
